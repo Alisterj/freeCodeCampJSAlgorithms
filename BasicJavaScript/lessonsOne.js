0c4dd2a65731534983ce0fca0ea8ad78a09719b5
Alisterj <lisarsiss@gmail.com>
2021-12-01 17:21:48 +0300

    LessonOne

---
+++ BasicJavaScript/lessonsOne.js
@@ -0,0 +1,9 @@
+//*Comment Your JavaScript Code
+
+//1) You should create a // style comment that contains at least five letters.
+
+//Alisterj
+
+//2) You should create a /* */ style comment that contains at least five letters.
+
+/*Alisterj*/
