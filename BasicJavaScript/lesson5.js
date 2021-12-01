62b74b7d4a1a96a3b8d4faf3a844803ba90c3e9f
Alisterj <lisarsiss@gmail.com>
2021-12-01 17:46:00 +0300

    Lessons four to five

---
+++ BasicJavaScript/lesson5.js
@@ -0,0 +1,9 @@
+//*Initializing Variables with the Assignment OperatorPassed
+/*It is common to initialize a variable to an initial value in the same line as it is declared.
+
+var myVar = 0;
+Creates a new variable called myVar and assigns it an initial value of 0.*/
+
+//!Define a variable a with var and initialize it to a value of 9.
+
+var a = 9;
